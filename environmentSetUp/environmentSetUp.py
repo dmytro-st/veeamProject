5532ce8ced295ca5598ff9363bf89039d0861f09
Dmytro Stashok <dmitriy.stashok@gmail.com>
2023-08-13 19:43:30 +0200

    few more tests added

--- environmentSetUp/environmentSetUp.py
+++ environmentSetUp/environmentSetUp.py
@@ -1,24 +1,20 @@
-import unittest
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-class EnvironmentSetUp(unittest.TestCase):
+class EnvironmentSetUp:
 
     def initialize(self):
-        global Instance
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--start-maximized")  # Maximize browser window
         chrome_options.add_argument("--disable-infobars")  # Disable info bars
         chrome_options.add_argument("--disable-extensions")  # Disable extensions
         chrome_options.add_argument("--disable-gpu")  # Disables GPU hardware acceleration
-        Instance = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-        Instance.implicitly_wait(5)
-        return Instance
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        self.driver.implicitly_wait(5)
+        return self.driver
 
     def close_driver(self):
-        global Instance
-        Instance.close()
-        Instance.quit()
+        self.driver.close()
+        self.driver.quit()
--- tests/testVacancies.py
+++ tests/testVacancies.py
@@ -8,12 +8,12 @@ from pageObjects.vacancies import Vacancies
 from verifiers.verifier import Verifier
 
 
-class VeeamVacanciesTest(EnvironmentSetUp):
+class VeeamVacanciesTest(EnvironmentSetUp, unittest.TestCase):
     def setUp(self):
-        self.driver = EnvironmentSetUp.initialize(self)
+        self.initialize()
         self.driver.get(const.BASE_URL)
 
-    def test_amount_of_vacancies(self):
+    def test_amount_of_rd_english_jobs(self):
         vacancies_page = Vacancies(self.driver)
         vacancies_page.accept_cookies()
         vacancies_page.department_select(department.RESEARCH_AND_DEVELOPMENT)
@@ -21,8 +21,22 @@ class VeeamVacanciesTest(EnvironmentSetUp):
         jobs = vacancies_page.get_amount_of_jobs()
         Verifier.compare_amount(jobs, 3)
 
+    def test_amount_of_all_german_jobs(self):
+        vacancies_page = Vacancies(self.driver)
+        vacancies_page.accept_cookies()
+        vacancies_page.language_select(language.GERMAN)
+        jobs = vacancies_page.get_amount_of_jobs()
+        Verifier.compare_amount(jobs, 1)
+
+    def test_amount_of_qa_jobs(self):
+        vacancies_page = Vacancies(self.driver)
+        vacancies_page.accept_cookies()
+        vacancies_page.department_select(department.QUALITY_ASSURANCE)
+        jobs = vacancies_page.get_amount_of_jobs()
+        Verifier.compare_amount(jobs, 13)
+
     def tearDown(self):
-        EnvironmentSetUp.close_driver(self)
+        self.close_driver()
 
 
 if __name__ == "__main__":
